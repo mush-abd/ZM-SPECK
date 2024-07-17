5e9390f360d330c5148b15088197c88b3436ea01
mush-abd <mushabd.22@gmail.com>
2024-07-18 04:13:48 +0530

    working encoder and decoder, 2.1

--- decoder.py
+++ decoder.py
@@ -2,77 +2,153 @@ import numpy as np
 from math import sqrt
 from PIL import Image as im 
 import cv2 as cv
-from matplotlib import pyplot as plt
 np.set_printoptions(threshold=np.inf)
+
+array = []
+
+def inverse_ProcessI(input_array, output_array, Npix, output_index, input_index, b):
+    # print('process I called')
+    if input_array[input_index] == 0:
+        for lambda_i in range(output_index, Npix):
+            if output_array[lambda_i] != 0:
+                output_array[lambda_i] = output_array[lambda_i] - (2**b)/2
+        # print(f'{output_index}')
+        output_index = Npix
+        # print(f'{output_index}')
+    input_index = input_index + 1
+    return output_index, input_index
+
+
+def inverse_EvalSL(output_index, lambda_):
+    while output_index & 3 * lambda_ == 0: 
+        # print(f'output index: {output_index}, lambda_: {lambda_}')
+        lambda_ = lambda_ * 4
+        # print(f'new lambda: {lambda_}')
+    return lambda_
+
+
+def inverse_PScan(input_array, input_index, output_array, output_index, b):
+    for lambda_i in range(output_index, output_index + 4):
+                if input_array[input_index] == 0:
+                    if output_array[lambda_i] != 0:
+                        output_array[lambda_i] = output_array[lambda_i] - (2**b)/2
+                    # if output_array[lambda_i] < 0:
+                    #     output_array[lambda_i] = output_array[lambda_i] + (2**b)/2
+                if input_array[input_index] == 1:
+                    if output_array[lambda_i] == 0:
+                        if input_array[input_index + 1] == 0:
+                            output_array[lambda_i] = (2**b * float(1.5))
+                        else:
+                            output_array[lambda_i] = -(2**b * float(1.5))
+                        input_index = input_index + 1
+                    else:
+                        # if output_array[lambda_i] > 0:
+                            output_array[lambda_i] = output_array[lambda_i] + (2**b)/2
+                        # if output_array[lambda_i] < 0:
+                            # output_array[lambda_i] = output_array[lambda_i] - (2**b)/2
+                input_index = input_index + 1
+
+    return output_index + 4, input_index
+
+def inverse_ProcessS(input_array, input_index, output_array, output_index, lambda_, b):
+    if input_array[input_index] == 0:
+        # print('Insignificant set')
+        for lambda_i in range(output_index, output_index + lambda_):
+            if output_array[lambda_i] != 0:
+                output_array[lambda_i] = output_array[lambda_i] - (2**b)/2 
+            # if output_array[lambda_i] < 0:
+            #     output_array[lambda_i] = output_array[lambda_i] + (2**b)/2  
+        output_index = output_index + lambda_
+        input_index = input_index + 1
+        # print(f'new output index: {output_index}')
+    else:
+        # print('Significant set')
+        if lambda_ > 4:
+            # print(f'lambda graeater than 4: {lambda_}')
+            lambda_ = lambda_ // 4
+            # print(f'new lambda: {lambda_}')
+            input_index = input_index + 1
+            return output_index, lambda_, input_index
+        else:
+            # print(f'Lambda is 4:')
+            input_index = input_index + 1
+            output_index, input_index = inverse_PScan(input_array, input_index, output_array, output_index, b)
+    
+    lambda_ =  inverse_EvalSL(output_index, lambda_)
+
+    return output_index, lambda_, input_index
 
 
 def main():
-
-    with open('binary_encoded.bin', 'rb') as file:
+    with open('cameraman.bin', 'rb') as file:
         data = file.read()
 
     input_string = ''
 
     for nums in data:
         bin_value = bin(nums)
-        # print(bin_value[2:])
         input_string = input_string + str(bin_value[2:].zfill(8))
-
-    
+    # print(input_string)
     input_array = []
 
     for i in input_string:
         i = int(i)
         input_array.append(i)
-
     print(f'size of input array: {len(input_array)}')
-    # print(len(input_array))
 
-    b = 7
-    Npix = 256 * 256
-    output = np.empty(Npix, dtype = 'int')
-    output.fill(0)
+    b = 10
+    Npix = 256*256
+    output_array = np.empty(Npix, dtype = 'int')
+    output_array.fill(0)
+    L = 3
 
-
-    j  = 0
-
+    input_index  = 0
+    lambda_root = Npix//4**L
+    # print(output)
     while b >= 0:
-    
-        print(f'for threshold: {2**b}')
-        for i in range (Npix):
-            if output[i] == 0:
-                if input_array[j] == 1:
-                    if input_array[j + 1] == 0:
-                        output[i] = (2**b * float(1.5))
-                    else:
-                        output[i] = -(2**b * float(1.5))
-                    j = j + 1
-            elif output[i] != 0:
-                if input_array[j] == 0:
-                    output[i] = output[i] - (2**b)/2
-                else:
-                    output[i] = output[i] + (2**b)/2
+        if (input_index >= len(input_array)):
+            break
+        print(f'For threshold: {2**b}')
+        # print(f'Input index starts at: {input_index}')
+        
+        lambda_ = lambda_root
+        output_index = 0
+        # print(f'Output index at start: {output_index}')
+        while output_index < Npix:
 
-            j = j + 1
+            
+            output_index, lambda_, input_index = inverse_ProcessS(input_array, input_index, output_array, output_index, lambda_, b)
+            # print(f'Returned from ProcessS\nOutput Index: {output_index}, Input Index: {input_index}, Lambda_: {lambda_}')
+            if output_index == lambda_ and output_index >= lambda_root  and output_index < Npix:
+                output_index, input_index = inverse_ProcessI(input_array, output_array, Npix, output_index, input_index, b)  
+                # print(f'Returned from ProcessI\nOutput Index: {output_index}, Input Index: {input_index}, Lambda_: {lambda_}')
+
         b = b - 1
-        print(f'next input index: {j}')
+        print(f'next input index: {input_index}')
     
-    np.save('decoder_output', output)
-    output_array = np.array(output)
+    np.save('result_decoder', output_array)
+    output_array = np.array(output_array)
+    print(output_array)
 
-    # print(output_array)
     a = int(sqrt(len(output_array)))
     image_matrix = np.reshape(output_array, (a, a))
+
+    from matplotlib import pyplot as plt
+
     image_matrix_normalized = cv.normalize(image_matrix, None, 0, 255, cv.NORM_MINMAX)
+
     # Converting to uint8 for proper image display
     image_matrix_uint8 = image_matrix_normalized.astype('uint8')
 
-    image = im.fromarray(image_matrix_uint8, 'L')
+    image = im.fromarray(image_matrix_uint8)
     image.show()
     image.save('reconstructed.png')
 
-    plt.imshow(image_matrix_uint8, cmap='gray', interpolation='nearest')
-    plt.show()
+    # plt.imshow(image_matrix_uint8, cmap='gray', interpolation='nearest')
+    # plt.show()
+
+
+
 
 
 if __name__ == '__main__':
--- encoder.py
+++ encoder.py
@@ -1,30 +1,26 @@
 import numpy as np
-import struct
 import math
 import cv2 as cv
 from numpy import array
 from PIL import Image as im
 np.set_printoptions(threshold=np.inf)
+import struct
 
+counter = 0
 def write_to_file(bin):
-    global arr
+    # global arr
+    global counter
+    
     while len(bin) < 8:
         bin = bin + '0'
+    # counter = counter + 8
     bin_int = int(bin, 2)
+    counter = counter + 8
     byte_num = struct.pack('B', bin_int)
-    with open('binary_encoded.bin', 'ab') as file:
+    with open('cameraman.bin', 'ab') as file:
         file.write(byte_num)
     bin = ''
     return bin
-
-
-# def image_to_array(image):
-#     gray =  cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-#     array2D = np.array(gray)
-#     image_array = array2D.flatten()
-#     np.save('image_coeffs', image_array)
-#     return image_array    
-
 
 def printKthBit(n, k):
     return((n & (1 << (k - 1))) >> (k - 1))
@@ -35,14 +31,12 @@ def Significance(coeffs,start_index, lambda_, T):
     max_abs = np.max(np.abs(coeffs[start_index: start_index + lambda_]))
     if max_abs < T:
         return 0
-    elif T <= max_abs < 2 * T:
-        return 1
     else:
-        return None
+        return 1
 
 
 def Significance_PScan(index, T):
-    # print(f'Significance Call')
+    # print(f'Significance PScan Call')
     if abs(index) < T:
         return 0
     elif T <= abs(index) < 2 * T:
@@ -50,130 +44,137 @@ def Significance_PScan(index, T):
     else:
         return None
 
+def Significance_I(coeffs, start_index, Npix, T):
+    # print(f'Significance I call')
+    max_abs = np.max(np.abs(coeffs[start_index: Npix]))
+    if max_abs < T:
+        return 0
+    else:
+        return 1
 
-def Process_I(coeffs, start_index, lambda_, T):
-    # print(f'Process I call')
-    output = Significance(coeffs, start_index, start_index  + lambda_, T)
-    if output == 0:
-        start_index = lambda_
+def Process_I(coeffs, start_index, Npix, T):
+    # print(f'Process_I call')
+    global output
+    significance = Significance_I(coeffs, start_index, Npix - 1, T)
+    # print(significance)
+    output = output + str(significance)
+    if len(output) == 8:
+        output = write_to_file(output)
+    if significance == 0:
+        return Npix
+    # print(output)
     return start_index
 
 
 def Pscan(coeffs, start_index, T, b):
+    # print(f'PSCan Call')
     global output
-    # print(f'PScan Call')
     for j in range(4):
         significance = Significance_PScan(coeffs[start_index + j], T)
-        # print(f'Significance : {significance}')
         if significance == 1 or significance == 0:
             output = output + str(significance)
-            # print(f'1. Significance {output}')
             if len(output) == 8:
                 output = write_to_file(output)
-
+                
         if significance == 1:
-
             if coeffs[start_index + j] < 0:
                 sign_bit = 1
             else:
                 sign_bit = 0
             output = output + str(sign_bit)
-            # print(f'2. Sign Bit {output}')
-            if len(output) == 8:
-                output = write_to_file(output)
-            # print("MSB:", msb)  # Output bth MSB of block
-        elif significance == None:
-            msb = printKthBit(coeffs[start_index + j], b + 1)
-            output = output + str(msb)
-            # print(f'3. MSB {output}')
             if len(output) == 8:
                 output = write_to_file(output)
 
+        elif significance == None:
+            msb = printKthBit(coeffs[start_index + j], b + 1)
+            output = output + str(msb)
+            if len(output) == 8:
+                output = write_to_file(output)
     return start_index + 4
 
 
 def EvalSL(start_index, lambda_):
-        # print(f'Process Eval SL call')
-        if start_index & 3 * lambda_ == 0:
+        # print(f'EvalSL call')
+        while start_index & 3 * lambda_ == 0: #check if 'if' or 'while'
             lambda_ = lambda_ * 4
+        # print(f'Lambda return from EvalSL: {lambda_}')
         return lambda_
+
     
 
 def process_S(coeffs, start_index, lambda_, T, b):
-
+    # print(f'Process_S call')
     global output
-    # print(f"In process S, Start Index: {start_index}, Size: {lambda_}, Threshold: {T}")
-    # print("Output Array:", output)
     significance = Significance(coeffs, start_index, lambda_, T)
-    # print(f'Signigicance: {significance}')
-    if significance == 0:
-        # print(f'significance start index {start_index}')
-        for i in range(start_index, start_index + lambda_):
-            output = output + str(significance)
-            # print(f'4. Significance {output}')
-            if len(output) == 8:
-                output = write_to_file(output)
-        start_index = start_index + lambda_
-        # print(f'start index return if significance == 0 : {start_index}')
+    # print(f'Significance: {significance}')
 
+    output = output + str(significance)
+    # print(f'output after: {output}')
+    if len(output) == 8:
+        output = write_to_file(output)
+    if significance == 0:
+        # print('Set skipped')
+        start_index = start_index + lambda_
     else:
         if lambda_ > 4:
             lambda_ = lambda_//4
-            # print(f'lambda gets divided: now -> {lambda_}')
+            # print(f'lambda_ becomes: {lambda_}')
             return start_index, lambda_
         else:
-            # print(f'Start Index before Pscan: {start_index}')
             start_index = Pscan(coeffs, start_index, T, b)
-            # print(f'Start index after Pscan: {start_index}')
 
     lambda_ = EvalSL(start_index, lambda_)
-    # print(f'lambda returned from eval SL: {lambda_}')
-    # print(f'updated block : {output}')
-    
+    # print(f'Return from Process_S, Start Index : {start_index}, Lambda: {lambda_}')
     return start_index, lambda_
 
 
 output = ''
 
 def main():
-
+    # lower_bound = -100  # Adjust the lower bound as needed
+    # upper_bound = 100 # Adjust the upper bound as needed
+    # coeffs = np.random.randint(lower_bound, upper_bound, size=64*64)
+    
     coeffs = np.load('array_coeffs.npy')
     print(coeffs.shape)
     Npix = len(coeffs)
 
-    
-    L = 4
+    L = 3
     lambda_root = Npix//4**L
-     
+
+    # print(f'Coeffs: {coeffs}')
     print("Npix:", Npix)
+    print(coeffs)
     
     # Initialize b and threshold
     initial_b = math.floor(math.log2(np.max(np.abs(coeffs))))
     b = initial_b
-    # print(f'bbbb {b}')  
+    print(f'b: {b}')  
     global output
     
-    open('binary_encoded.bin', 'wb').close()
-
-    #print(output)
+    open('cameraman.bin', 'wb').close()
+    # open('new.txt', 'w').close()
     
     while b >= 0:
+        print(f'Counter: {counter}')
         print(f'Threshold: {2**b}')
         start_index = 0
         lambda_ = lambda_root
         T = 2**b
-        
+        # print(type(start_index), type(Npix))
         while start_index < Npix:
-            # print(f'New Iteration')
+            # print(f'Starting Index: {start_index}')
             start_index, lambda_ = process_S(coeffs, start_index, lambda_, T, b)
-            if start_index == lambda_ and start_index >= lambda_root:
-                start_index = Process_I(coeffs, start_index, lambda_, T)
+            if start_index == lambda_ and start_index >= lambda_root and start_index < Npix:
+                # print(f'Process_I called from main')
+                start_index = Process_I(coeffs, start_index, Npix, T)
                 # print(f'start index returned from Process I: {start_index}')
             # print(f'Next Starting Index: {start_index}, Next Lambda: {lambda_}\n\n\n')
         
-        b = b - 1
+        b = b - 1 
 
     output = write_to_file(output)
+    print(f'Counter : {counter}')
 
 
 if __name__ == '__main__':
